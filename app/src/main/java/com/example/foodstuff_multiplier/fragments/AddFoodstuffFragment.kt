de607ee96f19ae5929d21260a57467f0d4c847c8
Otsuka Kohei <otsuka_kunikura@outlook.com>
2019-12-18 17:44:13 +0900

    食材追加画面を追加

---
+++ app/src/main/java/com/example/foodstuff_multiplier/fragments/AddFoodstuffFragment.kt
@@ -0,0 +1,24 @@
+package com.example.foodstuff_multiplier.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.foodstuff_multiplier.R
+
+class AddFoodstuffFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_foodstuff, container, false)
+    }
+
+    
+}
--- app/src/main/java/com/example/foodstuff_multiplier/fragments/InputDishNameFragment.kt
+++ app/src/main/java/com/example/foodstuff_multiplier/fragments/InputDishNameFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.example.foodstuff_multiplier.R
+import kotlinx.android.synthetic.main.fragment_input_dish_name.*
 
 class InputDishNameFragment : Fragment() {
 
@@ -16,5 +18,13 @@ class InputDishNameFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_input_dish_name, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        dishNameNextButton.setOnClickListener {
+            findNavController().navigate(R.id.action_inputDishNameFragment_to_addFoodstuffFragment)
+        }
     }
 }
