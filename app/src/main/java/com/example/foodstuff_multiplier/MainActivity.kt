d7608466bf90ed51aa923ef1a4011aa54434f634
Otsuka Kohei <otsuka_kunikura@outlook.com>
2019-12-18 16:33:16 +0900

    料理の一覧を表示する画面を追加

--- app/src/main/java/com/example/foodstuff_multiplier/MainActivity.kt
+++ app/src/main/java/com/example/foodstuff_multiplier/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.foodstuff_multiplier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_dish_list.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigationController = findNavController(navigationHostFragment)
+        val appBarConfiguration = AppBarConfiguration(navigationController.graph)
+        setupActionBarWithNavController(navigationController, appBarConfiguration)
+
+        addDishButton.setOnClickListener {
+            
+        }
     }
 }
