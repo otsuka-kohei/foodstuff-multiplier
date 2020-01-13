87df2be0c04895a1c69919a62fd9a733029f9ed9
Otsuka Kohei <otsuka_kunikura@outlook.com>
2020-01-13 12:16:09 +0900

    材料リストに入力がない場合は次の画面に遷移しないようにした

--- app/src/main/java/com/example/foodstuff_multiplier/fragments/AddFoodstuffFragment.kt
+++ app/src/main/java/com/example/foodstuff_multiplier/fragments/AddFoodstuffFragment.kt
@@ -1,10 +1,15 @@
 package com.example.foodstuff_multiplier.fragments
 
+import android.app.Activity
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.foodstuff_multiplier.AddFoodstuffListAdapter
 import com.example.foodstuff_multiplier.Foodstuff
@@ -18,6 +23,8 @@ class AddFoodstuffFragment : Fragment() {
 
     private val args: AddFoodstuffFragmentArgs by navArgs()
 
+    private var addNewDish: Boolean = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,6 +37,31 @@ class AddFoodstuffFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         val emptyFoodstuffItem = Foodstuff("", 0f, "")
+
+        if (args.dishName != null) {
+            addNewDish = true
+        }
+
+        addFoodstuffNextButton.setOnClickListener {
+            val foodstuffList: MutableList<Foodstuff> =
+                addFoodstuffListAdapter.getFoodStufItemList()
+            val validFoodStuffList: List<Foodstuff> =
+                foodstuffList.filter { it.name.isNotEmpty() && it.unit.isNotEmpty() }
+
+            if (validFoodStuffList.isNotEmpty()) {
+                val dishName = args.dishName ?: args.dish!!.name
+                if (dishName != null) {
+                    val action =
+                        AddFoodstuffFragmentDirections.actionAddFoodstuffFragmentToSelectMainFoodstuffFragment(
+                            dishName, validFoodStuffList.toTypedArray()
+                        )
+                    findNavController().navigate(action)
+                }
+            } else {
+                val dialog = AddFoodstuffValidationDialog(activity!!)
+                dialog.show(fragmentManager!!, null)
+            }
+        }
 
         val defaultFoodstuffList: List<Foodstuff> =
             args.dish?.foodstuffList ?: listOf(emptyFoodstuffItem, emptyFoodstuffItem)
@@ -52,5 +84,14 @@ class AddFoodstuffFragment : Fragment() {
             }
         addFoodStuffListView.adapter = addFoodstuffListAdapter
         addFoodStuffListView.setSelection(currentFoodstuffList.size + 1)
+    }
+}
+
+class AddFoodstuffValidationDialog(val activity: Activity) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        builder.setMessage("少なくとも1つ以上の材料名と単位を入力してください。")
+        builder.setPositiveButton("OK", null)
+        return builder.create()
     }
 }
